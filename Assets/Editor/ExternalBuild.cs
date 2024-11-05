23ad5063621b7cf4e6ec6472d3b719ca355478ba
HongYoseop <phudal@gravityneocyon.com>
2024-11-05 17:30:48 +0900

    임시 로거 제거 및 apk 이름을 현재 시간으로 변경

--- Assets/Editor/ExternalBuild.cs
+++ Assets/Editor/ExternalBuild.cs
@@ -2,33 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using System.IO;
+using System;
 
 public class ExternalBuild
 {
     public static void BuildCore()
     {
-        FileLogger.Log("Build Core");
-
         var sceneList = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
 
         BuildPipeline.BuildPlayer(
             sceneList.ToArray(),
-            "Builds/JenkinsTest.apk",
+            string.Format($"Builds/{DateTime.Now}.apk"),
             BuildTarget.Android,
             BuildOptions.None);
-    }
-}
-
-public static class FileLogger
-{
-    private static string logFilePath = Path.Combine(Application.persistentDataPath, "jenkins_debug.log");
-
-    public static void Log(string message)
-    {
-        using (StreamWriter writer = new StreamWriter(logFilePath, true))
-        {
-            writer.WriteLine($"{System.DateTime.Now}: {message}");
-        }
     }
 }
