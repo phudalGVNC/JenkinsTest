3edfced75613a9c00f4d830d4096ef212ca78873
HongYoseop <phudal@gravityneocyon.com>
2024-11-05 17:06:04 +0900

    임시 로거 추가

--- Assets/Editor/ExternalBuild.cs
+++ Assets/Editor/ExternalBuild.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using System.IO;
 
 public class ExternalBuild
 {
     public static void BuildCore()
     {
+        FileLogger.Log("Build Core");
+
         var sceneList = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
 
         BuildPipeline.BuildPlayer(
@@ -14,5 +17,18 @@ public class ExternalBuild
             "Builds/JenkinsTest.apk",
             BuildTarget.Android,
             BuildOptions.None);
+    }
+}
+
+public static class FileLogger
+{
+    private static string logFilePath = Path.Combine(Application.persistentDataPath, "jenkins_debug.log");
+
+    public static void Log(string message)
+    {
+        using (StreamWriter writer = new StreamWriter(logFilePath, true))
+        {
+            writer.WriteLine($"{System.DateTime.Now}: {message}");
+        }
     }
 }
