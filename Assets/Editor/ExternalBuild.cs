22bd5a4e5fed1d3a98d6a9eadb6391277bee16bb
HongYoseop <phudal@gravityneocyon.com>
2024-11-06 11:28:58 +0900

    매개변수 추가

--- Assets/Editor/ExternalBuild.cs
+++ Assets/Editor/ExternalBuild.cs
@@ -8,11 +8,45 @@ public class ExternalBuild
 {
     public static void BuildCore()
     {
+        string serverType = "Default";
+        string[] args = System.Environment.GetCommandLineArgs();
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-serverType" && i + 1 < args.Length)
+            {
+                serverType = args[i + 1];
+                break;
+            }
+        }
+
+        BuildCore_APK(serverType);
+    }
+
+    public static void BuildCore_APK(string serverType)
+    {
         var sceneList = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
+
+        PlayerSettings.Android.useAPKExpansionFiles = false;
+        EditorUserBuildSettings.buildAppBundle = false;
 
         BuildPipeline.BuildPlayer(
             sceneList.ToArray(),
-            string.Format($"Output/{DateTime.Now.Year}{DateTime.Now.Month.ToString("D2")}{DateTime.Now.Day.ToString("D2")}{DateTime.Now.Hour.ToString("D2")}{DateTime.Now.Minute.ToString("D2")}.apk"),
+            string.Format($"Output/{DateTime.Now.Year}{DateTime.Now.Month.ToString("D2")}{DateTime.Now.Day.ToString("D2")}{DateTime.Now.Hour.ToString("D2")}{DateTime.Now.Minute.ToString("D2")}_{serverType}.apk"),
+            BuildTarget.Android,
+            BuildOptions.None);
+    }
+
+    public static void BuildCore_AAB()
+    {
+        var sceneList = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
+
+        PlayerSettings.Android.useAPKExpansionFiles = true;
+        EditorUserBuildSettings.buildAppBundle = true;
+
+        BuildPipeline.BuildPlayer(
+            sceneList.ToArray(),
+            string.Format($"Output/{DateTime.Now.Year}{DateTime.Now.Month.ToString("D2")}{DateTime.Now.Day.ToString("D2")}{DateTime.Now.Hour.ToString("D2")}{DateTime.Now.Minute.ToString("D2")}.aab"),
             BuildTarget.Android,
             BuildOptions.None);
     }
